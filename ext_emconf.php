8a5b70d48039a845b0096ec00477cfbfd34bd8f0
Franz Holzinger <holzinger@ttproducts.de>
2018-05-02 14:18:35 +0200

    * adapt class map for TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@
 
 $EM_CONF[$_EXTKEY] = array (
   'title' => 'Static Methods since 2007',
-  'description' => 'This extension provides the migration classes for TYPO3 6.x and 7.x',
+  'description' => 'This extension provides the migration classes for TYPO3 8.x',
   'category' => 'misc',
   'shy' => 0,
-  'version' => '0.0.1',
+  'version' => '0.1.0',
   'dependencies' => '',
   'conflicts' => '',
   'suggests' => '',
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '5.5.0-7.99.99',
-      'typo3' => '6.2.0-7.99.99',
+      'typo3' => '8.7.0-8.99.99',
     ),
     'conflicts' =>
     array (
